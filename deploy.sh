d97f07df5a093b8d8f049535ffede6323db42dae
Dev01FC <frank@Franks-MacBook-Pro.local>
2019-03-29 20:33:06 +0100

    gh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,9 @@ git commit -m 'deploy'
  #git push -f git@github.com:frankleafy/dl58.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:Dev01FC/dlabdev.git master:gh-pages
+ #git push -f git@github.com:Dev01FC/dlabdev.git master:gh-pages
+ git push -f https://Dev01FC@github.com/Dev01FC/dlabdev.git master:gh-pages
+
+
 
 cd -
